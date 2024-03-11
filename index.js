2002742c6bce6a34558d5a5614ad0257bad6d334
Bilal Javed <bilal@gmail.com>
2024-03-11 14:20:57 -0400

    fourth commit print Salman ten times function added

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const  fs = require('fs')
 
-function printNameTenTimes() {
+function printBilalTenTimes() {
     for (let i = 0; i < 10; i++) {
       console.log("Bilal");
+    }
+  }
+
+  function printSalmanTenTimes() {
+    for (let i = 0; i < 10; i++) {
+      console.log("Salman");
     }
   }
